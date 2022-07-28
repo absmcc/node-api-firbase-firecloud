a75a224ae747149fab8656ad86d4d405b4f2888d
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-28 15:14:52 -0400

    Made a Cloud Funtion

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 
 
-```exports.helloThere = functions.https._onRequest((req, res) => {
+exports.helloThere = functions.https.onRequest((req, res) => {
     res.send('Hello the Firebase Cloud user!');
 })
 
